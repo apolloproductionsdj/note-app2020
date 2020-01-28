83362d781b774168fc5398d23fbea697b8801810
Mike Torres <apolloproductionsdj@gmail.com>
2020-01-28 01:30:01 +0000

    added the List component

---
+++ src/components/List.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class List extends React.Component {
+  render () {
+    return (
+      <div classassName="list-container">
+        List Component
+      </div>
+    );
+  }
+}
+
+
+export default List;
