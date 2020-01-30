c75075aa771f794ab6aad5ac2a7e358ca62ee15d
Mike Torres <apolloproductionsdj@gmail.com>
2020-01-30 07:24:26 +0000

    added ability to add a new note and then to edit note

--- src/App.js
+++ src/App.js
@@ -35,11 +35,19 @@ class App extends Component {
   }
 
 
-  submitNote = (data) => {
-    axios.post(urlFor('notes'), data)
+  submitNote = (data, id) => {
+    this.performSubmissionRequest(data, id)
     .then((res) => this.setState( { showNote: false }) )
     .catch((err) => console.log(err.response.data) );
 
+  }
+
+  performSubmissionRequest = (data, id) => {
+    if (id) {
+      return axios.patch(urlFor(`notes/${id}`), data);
+    } else {
+      return axios.post(urlFor('notes'), data);
+    }
   }
 
   render() {
