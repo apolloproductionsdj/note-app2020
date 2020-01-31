d51744e4760f816f82609b39d9f3621081b67fbf
Mike Torres <apolloproductionsdj@gmail.com>
2020-01-31 02:00:04 +0000

    added ability to delete tag from NoteCard.js

--- src/App.js
+++ src/App.js
@@ -73,6 +73,12 @@ class App extends Component {
     .catch((err) => console.log(err.response.dta) );
   }
 
+  deleteTag = (noteId, id) => {
+    axios.delete(urlFor(`/tags/${id}`))
+    .then((res) => this.getNote(noteId) ) 
+    .catch((err) => console.log(err.response.data));
+  }
+
   render() {
     const { showNote, notes, note, newTag } = this.state;
 
@@ -87,6 +93,7 @@ class App extends Component {
             newTag={newTag}
             closeTagForm={this.closeTagForm}
             submitTag={this.submitTag}
+            deleteTag={this.deleteTag}
           /> 
           :
           <List
