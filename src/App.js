d43d848698b31e41711b9da07d464093c4e871a5
Mike Torres <apolloproductionsdj@gmail.com>
2020-01-31 00:37:08 +0000

    made it so the add tag button is not there for new notes

--- src/App.js
+++ src/App.js
@@ -67,6 +67,12 @@ class App extends Component {
     this.setState({ newTag: false });
   }
 
+  submitTag = (data, noteId) => {
+    axios.post(urlFor(`notes/${noteId}/tagas`), data)
+    .then((res) => this.getNote(noteId) )
+    .catch((err) => console.log(err.response.dta) );
+  }
+
   render() {
     const { showNote, notes, note, newTag } = this.state;
 
@@ -80,6 +86,7 @@ class App extends Component {
             showTagForm={this.showTagForm}
             newTag={newTag}
             closeTagForm={this.closeTagForm}
+            submitTag={this.submitTag}
           /> 
           :
           <List
